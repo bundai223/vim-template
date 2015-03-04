6d33e2899d1dac6e74c7c9e01ed4709e4c7bc519
bundai223 <bundai223@gmail.com>
2015-03-04 17:56:14 +0900

    今使っているテンプレートを追加

---
+++ template/template.h
@@ -0,0 +1,12 @@
+//**********************************************************************
+//! @file   template.h
+//! @brief  describe
+//**********************************************************************
+ifndef _TEMPLATE_H_INCLUDED_
+#define _TEMPLATE_H_INCLUDED_
+
+
+
+
+#endif // _TEMPLATE_H_INCLUDED_
+
