abdca0a5a6ae0052e1ce4e26936c16fe69a76ab1
bundai223 <bundai223@gmail.com>
2015-03-04 18:01:50 +0900

    余計な改行を削除。 Cのヘッダは#pragma onceを使用するように変更

--- template/template.h
+++ template/template.h
@@ -1,12 +1,4 @@
 //**********************************************************************
-//! @file   template.h
 //! @brief  describe
 //**********************************************************************
-ifndef _TEMPLATE_H_INCLUDED_
-#define _TEMPLATE_H_INCLUDED_
-
-
-
-
-#endif // _TEMPLATE_H_INCLUDED_
-
+#pragma once
